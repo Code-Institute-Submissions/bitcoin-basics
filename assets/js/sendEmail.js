bab95eba724e11c9806b293372e7c6c793e1fa2b
shoreenb <shoreen.beckford@gmail.com>
2021-02-16 02:35:40 +0000

    emailjs sub form with variables working

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,23 @@
-console.log("hello");
+function sendMail(contactForm) {
+
+    console.log(contactForm.name.value);
+    console.log(contactForm.emailaddress.value);
+    console.log(contactForm.message.value);
+
+    emailjs.send("gmail", "bb", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "message": contactForm.message.value
+        })
+        .then(
+            function (response) {
+                $('#emailModalSuccess').modal('show');
+                console.log("SUCCESS", response);
+
+            },
+            function (error) {
+                $('#emailModalError').modal('show');
+                console.log("FAILED", error);
+            });
+    return false;
+}
