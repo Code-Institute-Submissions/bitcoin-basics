822dda68ef10d9bdc7f7dd94e1206adaf74e583d
shoreenb <shoreen.beckford@gmail.com>
2021-02-19 22:29:46 +0000

    Fix email form so response triggers success and error modals

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,26 +1,59 @@
 function sendMail(contactForm) {
 
-    console.log(contactForm.name.value);
-    console.log(contactForm.emailaddress.value);
-    console.log(contactForm.message.value);
+    //console.log(contactForm.name.value);
+    //console.log(contactForm.emailaddress.value);
+    //console.log(contactForm.message.value);
 
-    emailjs.send("gmail", "bb", {
-            "from_name": contactForm.name.value,
-            "from_email": contactForm.emailaddress.value,
-            "message": contactForm.message.value
-        })
-        .then(
-            function (response) {
-                $('#emailModalSuccess').modal('show');
-                console.log("SUCCESS", response);
+    /*const x = document.forms["#emailForm"]["name"].value;
 
-            },
-            function (error) {
-                $('#emailModalError').modal('show');
-                console.log("FAILED", error);
-            });
+   if (form.checkValidity() === false) {
+
+        $('#emailModalError').modal('show');
+        console.log("FAILED", error);
+        return false;
+
+    }*/
+    //const name = contactForm.name.value;
+    const email = document.getElementById("replyTo").value;
+    //const message = contactForm.message.value;
+
+    let isValid = ValidateEmail(email);
+
+    if (isValid) {
+        emailjs.send("gmail", "bb", {
+                "from_name": contactForm.name.value,
+                "from_email": contactForm.emailaddress.value,
+                "message": contactForm.message.value
+            })
+            .then(
+                function (response) {
+                    $('#emailModalSuccess').modal('show');
+                    console.log("SUCCESS", response);
+
+                },
+                function (error) {
+                    $('#emailModalError').modal('show');
+                    console.log("FAILED", error);
+                })
+        } else {
+            $('#emailModalError').modal('show');
+        }
     return false;
 }
+
+function ValidateEmail(email) {
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (email.match(mailformat)) {
+        $('#emailModalSuccess').modal('show');
+        console.log("SUCCESS is valid");
+        return true;
+    } else {
+        $('#emailModalError').modal('show');
+        console.log("FAILED");
+        return false;
+    }
+}
+
 
 (function () {
     'use strict';
