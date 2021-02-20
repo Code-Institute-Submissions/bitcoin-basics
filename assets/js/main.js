beff32bed86ede085cb1cad19250131714bfde80
shoreenb <shoreen.beckford@gmail.com>
2021-02-20 11:41:07 +0000

    Fix footer so it is in the correct position on all pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,7 @@ let currencyListCodes = document.getElementById("currencyList")
 
 
 
-const currencies = ["AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND",
-    "BOB", "BRL", "BSD", "BTN", "BWP", "BYR", "BZD", "CAD", "CDF", "CHF", "CLF", "CLP", "CNY", "COP", "CRC", "CUP", "CVE", "CZK", "DJF",
-    "DKK", "DOP", "DZD", "EEK", "EGP", "ERN", "ETB", "EUR", "FJD", "FKP", "GBP", "GEL", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL",
-    "HRK", "HTG", "HUF", "IDR", "ILS", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KPW", "KRW", "KWD",
-    "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LTL", "LVL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRO", "MTL", "MUR",
-    "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR",
-    "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "STD", "SVC", "SYP", "SZL", "THB", "TJS",
-    "TMT", "TND", "TOP", "TRY", "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VEF", "VND", "VUV", "WST", "XAF", "XAG", "XAU", "XBT",
-    "XCD", "XDR", "XOF", "XPF", "YER", "ZAR", "ZMK", "ZMW", "ZWL"
-];
+const currencies = ["AUD", "CAD", "CNY", "EUR", "GBP", "INR", "JMD", "JPY", "NGN", "RSD", "RUB", "USD", "XAG", "XAU", "XBT", "ZAR"];
 
 
 /*-------------------------Checkboxes---------------------------*/
@@ -102,11 +93,11 @@ function newsApi() {
 
     fetch('https://data.messari.io/api/v1/news/btc')
         .then((res) => {
-            console.log(res);
+            //console.log(res);
             return res.json()
         })
         .then((data => {
-            console.log(data);
+            //console.log(data);
             data.data.forEach(data => {
                 let li = document.createElement('li');
                 let a = document.createElement('a');
@@ -123,32 +114,4 @@ function newsApi() {
         btcNews.appendChild(li);
     }
 }
-console.log(newsApi());
-
-
-
-
-
-/*-------------------------Light/Dark Toggle---------------------------*/
-
-document.addEventListener("keyup", function (event) {
-
-    if (event.key == "l" || event.key == "L") {
-        lightTheme();
-    }
-    if (event.key == "d" || event.key == "D") {
-        darkTheme();
-    }
-});
-
-function lightTheme() {
-    el = document.getElementById("mainDiv");
-    el.classList.remove("dark");
-    el.classList.add("light");
-}
-
-function darkTheme() {
-    el = document.getElementById("mainDiv");
-    el.classList.remove("light");
-    el.classList.add("dark");
-}
+newsApi();
