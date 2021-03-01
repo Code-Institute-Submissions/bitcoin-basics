49f9a4f2cbb1a2ba7531d6cac65969c04d0ddb44
shoreenb <shoreen.beckford@gmail.com>
2021-03-01 23:24:22 +0000

    Add contact form validation tooltips back to form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -40,6 +40,23 @@ function ValidateMessage(message) {
     return false;
 }
 
-contactForm.addEventListener('submit', function () {
-    sendMail(this);
-})
+
+// contactForm.addEventListener('submit', function () {
+//     sendMail(this);
+// })
+
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        var forms = document.getElementsByClassName('needs-validation');
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
